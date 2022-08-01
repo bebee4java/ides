96c561f04115397f70762050885beb187aeddd1f
bebee4java <grsong.cn@gmail.com>
2022-08-01 23:43:07 +0800

    update common-utils version

--- engine/src/main/java/tech/ides/local/LocalIdesServiceApp.scala
+++ engine/src/main/java/tech/ides/local/LocalIdesServiceApp.scala
@@ -12,14 +12,14 @@ object LocalIdesServiceApp {
   def main(args: Array[String]): Unit = {
 
     IdesApp.main(Array(
-      "-spark.app.name", "ides",
-      "-spark.master", "local[*]",
-      "-spark.driver.memory", "512m",
-      "-ides.spark.service", "true",
-      "-ides.server.port", "9009",
-      "-ides.server.request-log.enable", "true",
-      "-ides.script.default.date.format", "yyyyMMdd",
-      "-ides.enableHiveSupport", "true"
+      "-spark.app.name", "ides"
+      , "-spark.master", "local[*]"
+      , "-spark.driver.memory", "512m"
+      , "-ides.spark.service", "true"
+      , "-ides.server.port", "9009"
+      , "-ides.server.request-log.enable", "true"
+      , "-ides.script.default.date.format", "yyyyMMdd"
+//      , "-ides.enableHiveSupport", "true"
     ))
   }
 
